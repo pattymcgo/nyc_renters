bc4f87d88b39ba2515eaa35e3b0efe50aee766d5
pattymcgo <pattymcgo@gmail.com>
2025-12-04 08:59:27 -0500

    Update get_pums.r

--- get_pums.r
+++ get_pums.r
@@ -1,8 +1,13 @@
 # Install and load packages
-install.packages(c("dplyr", "tidycensus", "tidyverse"), repos = "https://cloud.r-project.org")
+install.packages(c("dplyr", "tidycensus", "tidyverse", "googleCloudStorageR", "skimr", "dplyr", "bigrquery"), repos = "https://cloud.r-project.org")
 library(dplyr) 
 library(tidycensus)
 library(tidyverse)
+library(googleCloudStorageR)
+library(skimr) 
+library(dplyr) 
+library(bigrquery)
+
 # Download the 2023 1-year PUMS data for renters in New York state via Census API
 ny_pums <- get_pums(
   variables = c("PUMA", "PINCP", "COW", "INDP", "GRPIP"),
